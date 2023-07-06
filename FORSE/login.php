d26155f9f004d29698486213c2291560c327e20c
justinbns <138709175+justinbns@users.noreply.github.com>
2023-07-06 19:34:28 +0700

    Add files via upload

---
+++ FORSE/login.php
@@ -0,0 +1,37 @@
+<?php
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        require_once 'DatabaseSe.php';
+            $sql = "select * from userdata where username='$username'";
+            $response = mysqli_query($conn,$sql);
+            $result = array();
+            $result['login'] = array();
+
+            if(mysqli_num_rows($response) === 1){
+                $row = mysqli_fetch_assoc($response);
+                if(password_verify($password, $row['password'])){
+                    $index['userId'] = $row['userId'];
+                    $index['username'] = $row['username'];
+                    
+                    array_push($result['login'], $index);
+
+                    $result['success'] = "1";
+                    $result['message'] = "success";
+
+                    echo json_encode($result);
+                    mysqli_close($conn);
+            }
+            else{
+                $result['success'] = "0";
+                $result['message'] = "error";
+
+                echo json_encode($result);
+                mysqli_close($conn);
+            }
+        }
+    }
+
+?>
